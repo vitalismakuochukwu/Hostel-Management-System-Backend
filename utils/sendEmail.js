36d6dacf760493125a2e8a4536278a9fe3103dd6
vitalismakuochukwu <vitalismakuochukwu@gmail.com>
2025-12-28 18:00:15 +0100

    Update sendEmail.js

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -3,7 +3,7 @@ const nodemailer = require('nodemailer');
 const sendEmail = async (options) => {
   const transporter = nodemailer.createTransport({
     host: 'smtp.gmail.com',
-    port: 465,
+    port: 587,
     secure: true, // true for 465, false for other ports
     auth: {
       user: process.env.EMAIL_USERNAME,
