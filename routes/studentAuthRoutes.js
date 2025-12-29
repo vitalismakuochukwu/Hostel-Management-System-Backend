326a106e08a1c993653d75e6e0e9dcdd9c9b5d14
Yvitalismakuochukw <vitalismakuochukwu@gmail.com>
2025-12-29 02:06:51 +0100

    Fix 404: Added change-password route and controller

--- routes/studentAuthRoutes.js
+++ routes/studentAuthRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { registerUser, loginUser, verifyEmail, activateAccount, getUserProfile, updateUserProfile, resendCode, forgotPassword, resetPassword } = require('../controllers/authController');
+const { registerUser, loginUser, verifyEmail, activateAccount,  changePassword, getUserProfile, updateUserProfile, resendCode, forgotPassword, resetPassword } = require('../controllers/authController');
 
 router.post('/register', registerUser);
 router.post('/login', loginUser);
@@ -8,6 +8,7 @@ router.get('/verify/:token', verifyEmail);
 router.post('/activate', activateAccount);
 router.get('/profile', getUserProfile);
 router.put('/profile', updateUserProfile);
+router.post('/change-password', changePassword); 
 router.post('/resend-code', resendCode);
 router.post('/forgot-password', forgotPassword);
 router.post('/reset-password', resetPassword);
