94bc3cdb37a9abe7b61b5e4a1e2e4ae4ccd1ab5b
Yvitalismakuochukw <vitalismakuochukwu@gmail.com>
2025-12-30 05:27:13 +0100

    Backend: Fix 500 error by adding safety checks in verification logic

--- models/Admin.js
+++ models/Admin.js
@@ -15,10 +15,11 @@ const adminSchema = new mongoose.Schema({
 // Encrypt password using bcrypt
 adminSchema.pre('save', async function (next) {
   if (!this.isModified('password')) {
-    next();
+    return next();
   }
   const salt = await bcrypt.genSalt(10);
   this.password = await bcrypt.hash(this.password, salt);
+  next();
 });
 
 module.exports = mongoose.model('Admin', adminSchema);
